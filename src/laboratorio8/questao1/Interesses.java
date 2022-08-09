ef6c45bf464c40e2df123bf0d36362873a95c61f
Matheus Borges <matheus.borges@oobj.com.br>
2022-08-09 19:50:08 -0300

    abluablue

---
+++ src/laboratorio8/questao1/Interesses.java
@@ -0,0 +1,17 @@
+package laboratorio8.questao1;
+
+public class Interesses {
+    private String nome;
+
+    public Interesses(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
